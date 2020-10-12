58883d1fa070029b4a1a2b4c0b3ad1b1f0f0d325
Julian_Ehiem <jules.ehiem@gmail.com>
2020-10-12 18:42:46 -0400

    added file structures and back end

---
+++ cycloden-front/src/data.js
@@ -0,0 +1,34 @@
+export default {
+  products: [
+    {
+      _id: "1",
+      name: "Bike Pump",
+      category: "Shirts",
+      image: "https://source.unsplash.com/random/400x400",
+      price: 60,
+      brand: "Shimano",
+      rating: 4.5,
+      numReviews: 15,
+    },
+    {
+      _id: "2",
+      name: "Bike Gloves",
+      category: "Pants",
+      image: "https://source.unsplash.com/random/400x400",
+      price: 25,
+      brand: "Nike",
+      rating: 3.7,
+      numReviews: 3,
+    },
+    {
+      _id: "3",
+      name: "Helmet",
+      category: "Shirts",
+      image: "https://source.unsplash.com/random/400x400",
+      price: 125,
+      brand: "Tommaso",
+      rating: 4.9,
+      numReviews: 105,
+    },
+  ],
+};
